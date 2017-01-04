75c78dd0e710c8a698733bbc24fc35d3550e1cd5
edenadler <eden.n.adler@gmail.com>
2017-01-04 08:08:24 +0200

    trying to color grid boxes

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,73 +1,50 @@
-//"http://slavchoslavchev.com/tutorials/building-a-grid-matrix-in-javascript/
-(function(global){
+var game = {};
 
-	"use strict";
+game.init=(function(){
 
-	//assign cell x and y values that it occupies and give it a reference to the jQuery element that it represents
-	//cell constructor function
-	function Cell(config){
-		this.$el = config.$element;
-		this.x = config.x;
-		this.y = config.y;
+	for (var i = 0; i <20; i++){
+		$(".grid").append('<div class="row"></div>');
+		$(".row").eq(i).attr("data-x",i);
+
+		for (var j = 0; j<20; j++){
+			$(".row").eq(i).append('<div class="cell"></div>');
+			$(".row:eq("+i+") .cell").eq(j).attr("data-y",j);
+			$(".grid .row:eq("+i+") .cell:eq("+j+")").click(getCellAt);
+			}
 	}
-
-	//grid constructor function
-	function Grid(config){
-		this.grid=[]; //this will be a multidimensional array
-		this.cells = [];
-		this.rowsCount = config.rows;
-		this.colsCount = config.cols;
-		this.rows  =[];
-		this.cols = [];
-		if (config.render){
-			this.placeholder = config.render.placeholder;
-			this.render();
+	var getCellAt = function(x,y){
+		return $(".row:eq("+x+") .cell:eq("+y+")");
+	}
+	//sky
+	for (var i = 7; i<20; i++){
+		for (var j = 0; j<20; j++){
+			$(".grid").getCellAt(j,i).css('background-color', "#79a9f7");
 		}
 	}
 
-	Grid.prototype = {
-		createCell: function(config){
-			return new Cell(config);
-		},
-		getCellAt: function(x,y){
-			if (!this.grid[y]){
-				console.warn("No such Y coodinate");
-				return false;
-			}
-			if (!this.grid[y][x]){
-				console.warn("No such X coordinate");
-				return false;
-			}
-			return this.grid[y][x];
-		},
-		render: function(options){
-			if(options && options.placeholder){
-				this.placeholder = options.placeholder;
-			}
-			this.$placeholder = $(this.placeholder);
-			if (!this.placeholder || this.$placeholder.length === 0){
-				console.error('Placeholder is not present');
-				return;
-			}
-			var i, j, $row, $cell, cell, cellId = 0;
-			for (i = 0; i<this.rowsCount; i++){
-				this.grid[i] = [];
-				$row = $('<div class="row"></div>').prependTo(this.$placeholder);
-				for (j = 0; j<this.colsCount; j++){
-					$cell = $('<div class="cell"></div>').appendTo($row);
-					cell = this.createCell({$element: $cell, x: j, y:i});
-					this.grid[i].push(cell);
-					this.cells.push(cell);
-				}
-			}
-			//rows
-			var self = this;
-			this.grid.forEach(function(row){
-				self.rows.push(row);
-			});
-		}
-	};
+});
 
-	global.Grid = Grid;
+// game.getCellAt = function(x,y){
+// 	return $(".row:eq("+x+") .cell:eq("+y+")");
+// }
+// //sky
+// for (var i = 7; i<20; i++){
+// 	for (var j = 0; j<20; j++){
+// 		$(".grid").game.getCellAt(j,i).css('background-color', "#79a9f7");
+// 	}
+// }
 
-}(window));
+game.clickOnTools = function(){
+
+};
+
+game.clickOnBoxes = function(){
+
+};
+
+game.renderBoard = function(){
+
+};
+
+
+game.init();
